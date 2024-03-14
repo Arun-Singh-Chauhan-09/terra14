0d826366099728342e1f3a50973aa048002ca867
root <root@Sosuv>
2024-03-14 16:10:16 +0530

    arun change

--- test-2/security.tf
+++ test-2/security.tf
@@ -12,5 +12,36 @@ terraform {
 }
 
 resource "aws_security_group" "test-2" {
+
+vpc_id      = "vpc-075bb5722eee6e160"
+name        = "test-2"
+description = "test-2"
+ingress= [
+              {
+                "cidr_blocks"= [ "0.0.0.0/0", ]
+                description= ""
+                from_port= 0
+                ipv6_cidr_blocks= []
+                prefix_list_ids= []
+                protocol= -1
+                security_groups= []
+                self= false
+                to_port= 0
+              }
+		]     
+
+egress= [
+              {
+                "cidr_blocks"= [ "0.0.0.0/0", ]
+                description= ""
+                from_port= 0
+                ipv6_cidr_blocks= []
+                prefix_list_ids= []
+                protocol= -1
+                security_groups= []
+                self= false
+                to_port= 0
+              }
+		]     
            
 }
